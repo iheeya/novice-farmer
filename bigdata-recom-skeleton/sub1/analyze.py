5f5c946159e6291c6116ca5f5e85cca604b9f138
최다희 <chdh8987@naver.com>
2024-08-29 10:50:59 +0900

    Req. 2-4
    Former-commit-id: db03ec992a6fb8de97ae10801af308eb17ccf9c3

--- bigdata-recom-skeleton/sub1/analyze.py
+++ bigdata-recom-skeleton/sub1/analyze.py
@@ -27,7 +27,7 @@ def sort_stores_by_score(dataframes, n=20, min_reviews=30):
     scores = filtered_scores_group.groupby(["store", "store_name"]).score.mean()
     scores = scores.sort_values(ascending=False)
 
-
+  
     return scores.head(n=n).reset_index()
 
 
@@ -43,17 +43,16 @@ def get_most_reviewed_stores(dataframes, n=20):
     )
     review_group = stores_reviews.groupby(["store", "store_name"])
 
-
+    
     # 그룹별 리뷰 개수 계산
     review_counts_group = review_group.size()
 
 
     most_reviewed_stores = review_counts_group.sort_values(ascending=False)
-
-    print(most_reviewed_stores.head(n=n))
-
+   
 
     return most_reviewed_stores.head(n=n).reset_index()
+
 
 
  
@@ -63,7 +62,36 @@ def get_most_active_users(dataframes, n=20):
     """
     Req. 1-2-4 가장 많은 리뷰를 작성한 `n`명의 유저를 정렬하여 리턴합니다.
     """
+    # stores_reviews = pd.merge(
+    #     dataframes["stores"], dataframes["reviews"], left_on="id", right_on="store"
+    # )
+    # review_group = stores_reviews.groupby(["store", "store_name"])
+
+    reviews = pd.merge(
+        dataframes["users"], dataframes["reviews"], left_on="id", right_on="user"
+    )
+
+    review_group = reviews.groupby(["user", "store"])
+
+    # # 그룹별 리뷰 개수 계산
+    # review_counts_group = review_group.size()
+
+    user_counts_group = review_group.size()
+
+
+    # most_reviewed_stores = review_counts_group.sort_values(ascending=False)
+    most_reviewed_users = user_counts_group.sort_values(ascending=False)
+
+    # print(f'most_reviewed_users: {most_reviewed_users}')
+
+    return most_reviewed_users.head(n=n).reset_index()
+    # return most_reviewed_stores.head(n=n).reset_index()
+
+
     raise NotImplementedError
+
+
+
 
 
 def main():
@@ -72,9 +100,11 @@ def main():
     term_w = shutil.get_terminal_size()[0] - 1
     separater = "-" * term_w
 
+    # print(f'data: {data}')
+
     stores_most_scored = sort_stores_by_score(data)
     get_most_reviewed_stores(data)
-  
+    get_most_active_users(data)
 
     print("[최고 평점 음식점]")
     print(f"{separater}\n")
