9f3459d2d4dc514ca5a41ef46e47c8333c4550ce
김호경 <ssafy1123992@gmail.com>
2024-08-30 13:01:18 +0900

    Req 4
    Former-commit-id: ac34b069d79c2af5737344fc30d38ccbf41b38a8

--- bigdata-recom-skeleton/sub1/matrix.py
+++ bigdata-recom-skeleton/sub1/matrix.py
@@ -20,7 +20,6 @@ def make_user_store_matrix(dataframes):
         os.remove(file_path)
         print(f"{file_path} 파일이 삭제되었습니다.")
         
-    # 'reviews' 데이터프레임을 사용하여 유저-음식점 행렬 생성
     reviews = pd.DataFrame(dataframes['reviews'])
     
     user_store_matrix = reviews.pivot_table(index='user', columns='store', values='score')
@@ -41,23 +40,19 @@ def make_user_category_matrix(dataframes):
     유저-카테고리 행렬 생성
     유저와 음식점 카테고리를 축으로 하고 평점 평균을 값으로 갖는 행렬을 만들어 저장.
     '''
-    reviews = pd.DataFrame(dataframes['reviews'])  # 리뷰 데이터
-    stores = pd.DataFrame(dataframes['stores'])  # 음식점 데이터
+    reviews = pd.DataFrame(dataframes['reviews'])
+    stores = pd.DataFrame(dataframes['stores'])
     
-    # 음식점별 평균 평점 계산
     store_avg_ratings = reviews.groupby('store_id')['rating'].mean().reset_index()
     store_avg_ratings.columns = ['store_id', 'average_rating']
     
-    # 음식점 데이터와 평균 평점 데이터 병합
     store_data_with_ratings = pd.merge(stores, store_avg_ratings, on='store_id')
     
-    # 카테고리별로 그룹화하여 평균 평점 계산
     category_avg_ratings = store_data_with_ratings.groupby('category')['average_rating'].mean().reset_index()
     
-    # NaN 값이 있을 경우 처리 (필요한 경우)
     category_avg_ratings = category_avg_ratings.fillna(0)
     print(category_avg_ratings.head())
-    category_avg_ratings.to_csv('category_avg_ratings.csv', index=False)
+    category_avg_ratings.to_csv('유저-카테고리 행렬.csv', index=False)
     
     
 def main():
