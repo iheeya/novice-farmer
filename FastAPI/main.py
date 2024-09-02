9ff64ab810a7eb27bd6eed9890991d9b792e5dfb
김호경 <ssafy1123992@gmail.com>
2024-09-02 17:49:22 +0900

    FastAPI 생성
    Former-commit-id: 5747413616bef93ca98672ac37aaba6f81765593

---
+++ FastAPI/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/munang")
+async def root():
+    return {"message": "7계명"}
