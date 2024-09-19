972b845132e40e94cc03bf8332bd45efa31e68a6
hajae <gkrhf@naver.com>
2024-09-19 11:21:06 +0900

    [BE]feat: 지번주소로 위경도 찾기 기능 구현
    Former-commit-id: 31c60e8df89da06c546c4e2cf78cb28e821bc342

--- BE/farmer/src/main/java/com/d207/farmer/service/farm/FarmService.java
+++ BE/farmer/src/main/java/com/d207/farmer/service/farm/FarmService.java
@@ -6,6 +6,7 @@ import com.d207.farmer.domain.farm.UserPlace;
 import com.d207.farmer.domain.place.Place;
 import com.d207.farmer.domain.plant.Plant;
 import com.d207.farmer.domain.user.User;
+import com.d207.farmer.dto.farm.api.GeoAPIResponseDTO;
 import com.d207.farmer.dto.farm.register.FarmRegisterRequestDTO;
 import com.d207.farmer.repository.farm.FarmRepository;
 import com.d207.farmer.repository.farm.UserPlaceRepository;
@@ -14,9 +15,15 @@ import com.d207.farmer.repository.plant.PlantRepository;
 import com.d207.farmer.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 @Slf4j
@@ -30,6 +37,12 @@ public class FarmService {
     private final PlantRepository plantRepository;
     private final UserRepository userRepository;
     private final UserPlaceRepository userPlaceRepository;
+
+    @Value("${external.api.naver.apigw.id}")
+    private String apigwId;
+
+    @Value("${external.api.naver.apigw.key}")
+    private String apigwKey;
 
     @Transactional
     public String registerFarm(Long userId, FarmRegisterRequestDTO request) {
@@ -51,9 +64,9 @@ public class FarmService {
 
     private UserPlace createUserPlace(User user, Place place, FarmRegisterRequestDTO request) {
         // 위 경도 불러오기
-        // TODO 주소로 위경도 찾기
-        String latitude = "";
-        String longitude = "";
+        Map<String, String> latAndLongMap = getLatAndLongByJibun(request.getPlace().getAddress().getJibun());
+        String latitude = latAndLongMap.get("latitude");
+        String longitude = latAndLongMap.get("longitude");
 
         Address address = request.getPlace().getAddress();
         // 내장 값타입은 값 변경할 때 생성자로 인스턴스 새로 생성
@@ -64,11 +77,45 @@ public class FarmService {
         return new UserPlace(user, place, latitude, longitude, place.getName(), newAddress, request.getPlace().getDirection());
     }
 
+    private Map<String, String> getLatAndLongByJibun(String query) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 요청 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-NCP-APIGW-API-KEY-ID", apigwId);
+        headers.set("X-NCP-APIGW-API-KEY", apigwKey);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // 파라미터 포함 url 생성
+        String url = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + query;
+
+        // 요청 및 응답
+        ResponseEntity<GeoAPIResponseDTO> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                GeoAPIResponseDTO.class
+        );
+
+
+        // 응답 처리
+        if(response.getStatusCode().is2xxSuccessful()) {
+            Map<String, String> result = new HashMap<>();
+            result.put("latitude", response.getBody().getAddresses().get(0).getY());
+            result.put("longitude", response.getBody().getAddresses().get(0).getX());
+            return result;
+        } else {
+            throw new IllegalStateException("네이버 지오코딩 api 호출 실패");
+        }
+
+    }
+
     private Address createAddress(Address addr) {
         // 번지는 지번의 마지막 토큰 값으로 넣기
         // 우편번호 api에서 번지는 제공해주지 않음
         // 대신 띄어쓰기 형식이 핏하게 맞아서 토크나이저로 마지막 토큰만 가져오면 됨
-
         String jibun = addr.getJibun();
         StringTokenizer st = new StringTokenizer(jibun, " ");
         String bunji = "";
