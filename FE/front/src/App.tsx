08a4de2dcdbf2ef307e828e1c832f65e1095f55d
KangDongwan <douxsouvenir12@gmail.com>
2024-09-19 17:06:42 +0900

    [FE]feat : Footer 구현
    Former-commit-id: b4bee650b7af307978c00649d7881b9b58148daa

--- FE/front/src/App.tsx
+++ FE/front/src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/Home/Main";
 import Test from "./pages/test";
-
-function Main() {
-  return <div>메인페이지</div>;
-}
+import Footer from "./components/Footer";  // 푸터 컴포넌트 임포트
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path="/test" element={<Test />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+
+        {/* 푸터는 모든 페이지 하단에 공통적으로 보임 */}
+        <Footer />
+      </div>
     </Router>
   );
 }
---
+++ FE/front/src/pages/Home/Main.tsx
@@ -0,0 +1,13 @@
+// pages/Home/Main.tsx
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>메인 페이지</h1>
+      <p>여기에 메인 페이지 내용을 추가합니다.</p>
+    </div>
+  );
+};
+
+export default Main;
