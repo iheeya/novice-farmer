30b2f610e81c89e60ce01692323c157e2c2bad32
최다희 <chdh8987@naver.com>
2024-09-20 10:17:31 +0900

    [FE]이미지 선호도, 서비스 나타내기
    Former-commit-id: 7bf32b72527cb4af2243dc9ed587fc7053464081

--- FE/front/src/components/RegisterGarden/gardenSelect.tsx
+++ FE/front/src/components/RegisterGarden/gardenSelect.tsx
@@ -5,6 +5,7 @@ import school from '../../assets/farms/4.png'
 import rooftop from '../../assets/farms/5.png'
 import farmPlace from '../../assets/dummydata/farmPlace.json'
 import '../../styles/RegisterGarden/gardenSelect.css'
+import { FaHeart } from "react-icons/fa";
 
 
 // farm/place api 사용해서 텃밭 리스트 가져오기
@@ -19,19 +20,23 @@ function GardenSelect() {
     }
 
     return(
-        <>
+        <div className='frame'>
             <div className='farm-instruction'>텃밭을 선택해주세요!</div>
             <div className='image-group'>
                 {farmPlace.map(place => (
+                    <div className={`image-container ${place.isService ? '': 'blur'}`} key={place.placeId}>      {/* 서비스하지 않는 텃밭은 흑백 처리 */}
+                     {place.isFavorite && <FaHeart className='heart-icon' />}
+                     {/* 왼쪽 조건이 True일 때만 오른쪽에 있는 값을 반환 */}
                     <img
-                        key={place.placeId}
                         src={imageMapping[place.placeId]}
                         alt={`${place.placeName} 이미지`}
                         className='image-size'
                     />
+                    <div className='farm-name'>{place.placeName}</div>
+                </div>
                 ))}
             </div>
-        </>
+        </div>
     )
 }
 
