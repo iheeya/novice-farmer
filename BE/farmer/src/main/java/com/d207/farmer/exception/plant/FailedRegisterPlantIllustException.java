36a2573c1f45641322a57aee9f5c3371516bbe9c
hajae <gkrhf@naver.com>
2024-09-22 13:30:10 +0900

    [BE]feat: 작물 일러스트 등록 api 구현
    Former-commit-id: de25be4b1a45d26f221ec2686bf54b677db7830f

---
+++ BE/farmer/src/main/java/com/d207/farmer/exception/plant/FailedRegisterPlantIllustException.java
@@ -0,0 +1,10 @@
+package com.d207.farmer.exception.plant;
+
+public class FailedRegisterPlantIllustException extends RuntimeException {
+    public FailedRegisterPlantIllustException() {
+    }
+
+    public FailedRegisterPlantIllustException(String message) {
+        super(message);
+    }
+}
--- BE/farmer/src/main/java/com/d207/farmer/service/user/MyPlantService.java
+++ BE/farmer/src/main/java/com/d207/farmer/service/user/MyPlantService.java
@@ -54,6 +54,7 @@ public class MyPlantService {
     public String waterPlant(Long userId, ManagePlantRequestDTO request) {
         Farm farm = farmRepository.findById(request.getFarmId()).orElseThrow();
         // TODO todo에 추가가 아니라 원래 있던 todo를 update 해야할 것 같음
+        // TODO 원래 최근에 있던 물주기 todo를 업데이트하거나, 없으면 새로 추가
 
         return "작물 물주기 성공";
     }
@@ -62,6 +63,7 @@ public class MyPlantService {
     public String fertilizerPlant(Long userId, ManagePlantRequestDTO request) {
         Farm farm = farmRepository.findById(request.getFarmId()).orElseThrow();
         // TODO todo에 추가가 아니라 원래 있던 todo를 update 해야할 것 같음
+        // TODO 원래 최근에 있던 비료주기 todo를 업데이트하거나, 없으면 새로 추가
         return "작물 비료주기 성공";
     }
 
