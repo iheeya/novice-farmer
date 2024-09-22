f077f31f01c9999d22937e88d8d57d699e78a47b
hajae <gkrhf@naver.com>
2024-09-22 11:45:13 +0900

    [BE]feat: 텃밭 이름 변경 api 구현
    Former-commit-id: ab2db078ef81c5d48817b48da7caeaad1b6245ce

--- BE/farmer/src/main/java/com/d207/farmer/service/user/MyPlaceService.java
+++ BE/farmer/src/main/java/com/d207/farmer/service/user/MyPlaceService.java
@@ -1,8 +1,10 @@
 package com.d207.farmer.service.user;
 
+import com.d207.farmer.domain.farm.UserPlace;
 import com.d207.farmer.dto.myplace.MyPlaceRequestDTO;
 import com.d207.farmer.dto.myplace.MyPlaceResponseDTO;
 import com.d207.farmer.dto.myplace.UpdateMyPlaceNameRequestDTO;
+import com.d207.farmer.repository.farm.UserPlaceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,13 +15,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MyPlaceService {
+
+    private final UserPlaceRepository userPlaceRepository;
+
     public MyPlaceResponseDTO getMyPlace(Long userId, MyPlaceRequestDTO request) {
         // TODO
         return null;
     }
 
     public String updateMyPlaceName(Long userId, UpdateMyPlaceNameRequestDTO request) {
-        // TODO
-        return null;
+        UserPlace userPlace = userPlaceRepository.findById(request.getUserPlaceId()).orElseThrow();
+        userPlace.modifyName(request.getUserPlaceName());
+        return "장소 이름 변경 성공";
     }
 }
