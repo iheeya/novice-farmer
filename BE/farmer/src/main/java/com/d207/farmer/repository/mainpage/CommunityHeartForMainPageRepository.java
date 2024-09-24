014b884e5a6658ca9ed579d9e8b4e215f25349f4
hajae <gkrhf@naver.com>
2024-09-24 21:55:49 +0900

    [BE]chore: community 컴포넌트 api 구현중
    Former-commit-id: c266803bf9d9934e5489b15e510f68007cccadfc

--- BE/farmer/src/main/java/com/d207/farmer/repository/mainpage/CommunityHeartForMainPageRepository.java
+++ BE/farmer/src/main/java/com/d207/farmer/repository/mainpage/CommunityHeartForMainPageRepository.java
@@ -2,9 +2,12 @@ package com.d207.farmer.repository.mainpage;
 
 import com.d207.farmer.domain.community.CommunityHeart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommunityHeartForMainPageRepository extends JpaRepository<CommunityHeart, Long> {
-    List<CommunityHeart> findByCommunityIdIn(List<Long> communityIds);
+    @Query("select ch from CommunityHeart ch join fetch ch.community where ch.community.id in :communityIds")
+    List<CommunityHeart> findByCommunityIdIn(@Param("communityIds") List<Long> communityIds);
 }
--- BE/farmer/src/main/java/com/d207/farmer/repository/mainpage/CommunitySelectedTagForMainPageRepository.java
+++ BE/farmer/src/main/java/com/d207/farmer/repository/mainpage/CommunitySelectedTagForMainPageRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommunitySelectedTagForMainPageRepository extends JpaRepository<CommunitySelectedTag, Long> {
-    @Query("select cst from CommunitySelectedTag cst join fetch cst.community where cst.communityTag.id = :communityTagId and cst.community.checkDelete = false " +
+    @Query("select cst from CommunitySelectedTag cst join fetch cst.community join fetch cst.community.user where cst.communityTag.id = :communityTagId and cst.community.checkDelete = false " +
             " order by cst.community.writeDate desc")
     List<CommunitySelectedTag> findByCommunityTagId(@Param("communityTagId") Long communityTagId);
 }
