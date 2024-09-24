cb33d744e72382f6b1806dd65a5047162dbc6ab6
김호경 <ssafy1123992@gmail.com>
2024-09-24 18:00:29 +0900

    [DT] Calculating of Growth Variable DDs
    Former-commit-id: b4a5e3a93cdffcdd45ee5b8eb78e2cff5611f7f5

--- DT/app/main.py
+++ DT/app/main.py
@@ -1,5 +1,6 @@
 # main.py
 from fastapi import FastAPI
+import math
 
 app = FastAPI()
 
@@ -13,7 +14,51 @@ def read_item(item_id: int, q: str = None):
 
 @app.get("/data-api/plant/growth")
 def crops_growth():
-    return {"message": "Crops Success"}
+    thi, tlow = 33.33, 7.22
+    def docalcs_S(max_val, min_val):
+        if min_val > thi:
+            heat = thi - tlow
+        else:
+            if max_val <= tlow:
+                heat = 0
+            else:
+                fk1 = 2 * tlow
+                diff = max_val - min_val
+                sum_val = max_val + min_val
+                if min_val >= tlow:
+                    heat = (sum_val - fk1) / 2
+                else:
+                    heat = sinec(sum_val, diff, fk1)
+                if max_val > thi:
+                    fk1 = 2 * thi
+                    heat -= sinec(sum_val, diff, fk1)
+        
+        ddtmp = heat / 2
+        return ddtmp
+
+    def sinec(sum_val, diff, fk1):
+        twopi = 6.28318530717959
+        pihlf = 1.5707963267949
+        d2 = fk1 - sum_val
+        d3 = diff**2 - d2**2
+        # 부동소수점 오차로 인한 sqrt 오류 방지
+        if d3 < 0 and d3 > -1e-9:
+            d3 = 0
+        theta = math.atan2(d2, math.sqrt(d3))
+        if d2 < 0 and theta > 0:
+            theta -= 3.1416
+        heat = (diff * math.cos(theta) - d2 * (pihlf - theta)) / twopi
+        return heat
+
+    tmax, tmin = map(float, input().split())
+    DD1 = 2*docalcs_S(tmax, tmin)
+    tmep_tlow, temp_thi = tlow, tmin
+    tlow, thi = thi, 2*thi - tlow
+    DD2 = 2*docalcs_S(tmax, tmin)
+    tlow, thi = tmep_tlow, temp_thi
+
+    DDs = round((DD1-DD2) * (9/5), 2)
+    return { 'DDs' : DDs}
 
 @app.get("/data-api/plant/pest")
 def pest_diagnosis():
