54b4e148c8ec663a5fd81d88f2e875d6b59f6f0c
최다희 <chdh8987@naver.com>
2024-09-26 14:19:01 +0900

    [FE]feat: 텃밭기반 작물 추천 post api 연결
    Former-commit-id: 207431b1c9b22309fa4f9941ab1507df2c4a6d6c

--- FE/front/src/pages/AddGarden/registerGarden.tsx
+++ FE/front/src/pages/AddGarden/registerGarden.tsx
@@ -22,6 +22,7 @@ function RegisterGarden(){
   useEffect(() => {
     if (isLoading) {
       // 2초 후 로딩을 완료하고, 추천 데이터를 확인 후 상태 설정
+      console.log('메인 페이지 resonse 확인', response)
       setTimeout(() => {
         setIsLoading(false);  // 로딩 완료
         if (response && response.length > 0) {
@@ -29,7 +30,7 @@ function RegisterGarden(){
         }
       }, 2000);  // 2초 지연 후 처리
     }
-  }, [isLoading]);  // isLoading 상태가 변할 때만 실행
+  }, [isLoading, response]);  // isLoading 상태가 변할 때만 실행
 
     
 
--- FE/front/src/services/AddGarden/AddGardenPost.tsx
+++ FE/front/src/services/AddGarden/AddGardenPost.tsx
@@ -10,6 +10,18 @@ interface GardenPayload {
     address: Address; // Address 인터페이스 사용
 }
 
+interface Final{
+    place: {
+        placeId: number | null;
+        address: Address;
+    };
+    plant: {
+        plantId: number | null; // 또는 필요에 따라 다른 타입
+        myPlantName: string;
+        memo?: string; // 선택적으로 memo가 있을 수 있음
+    };
+}
+
 export function selectGardenPost(payload:GardenPayload){
     return api
         .post('farm/plant/recommend', payload)
@@ -17,6 +29,18 @@ export function selectGardenPost(payload:GardenPayload){
             return Promise.resolve(response.data)
         })
         .catch((e)=> {
+            return Promise.reject(e)
+        })
+}
+
+
+export function gardenFinishPost(payload:Final){
+    return api
+        .post('farm', payload)
+        .then((response) => {
+            return Promise.resolve(response.data)
+        })
+        .catch((e) => {
             return Promise.reject(e)
         })
 }
