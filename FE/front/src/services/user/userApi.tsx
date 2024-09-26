d4be14fb53025ac34dad08c61cf1396c4fa60a4c
김도환 <rlaehghkr@gmail.com>
2024-09-26 09:57:17 +0900

    solve conflict
    Former-commit-id: 622503996bbdeec329d0d706e05c4b8d57c3570c

---
+++ FE/front/src/services/user/userApi.tsx
@@ -0,0 +1,122 @@
+import api from "../../utils/axios";
+
+interface HandleLoginProps {
+  email: string;
+  password: string;
+}
+
+interface HandleSignupProps {
+  email: string;
+  password: string;
+  nickname: string;
+  age: number;
+  gender: string;
+  address: string;
+  pushAllow: boolean;
+}
+
+interface LoginResponse {
+  firstLogin: boolean;
+}
+
+interface postSurveyInfoProps {
+  plant: { id: number }[];
+  place: { id: number }[];
+}
+
+// 로그인 함수
+export function handleLogin({ email, password }: HandleLoginProps): Promise<LoginResponse> {
+  return api
+    .post("/user/login", {
+      email,
+      password,
+    })
+    .then((response) => {
+      const { firstLogin, accessToken, refreshToken } = response.data;
+      sessionStorage.setItem("accessToken", accessToken);
+      sessionStorage.setItem("refreshToken", refreshToken);
+      return Promise.resolve({ firstLogin });
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+}
+
+// 회원가입 함수
+export function handleSignup({
+  email,
+  password,
+  nickname,
+  age,
+  gender,
+  address,
+  pushAllow,
+}: HandleSignupProps): Promise<void> {
+  return api
+    .post("/user", {
+      email,
+      password,
+      nickname,
+      age,
+      gender,
+      address,
+      pushAllow,
+    })
+    .then((response) => {
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+}
+
+// 설문조사 리스트 get 함수
+export function getSurveyInfo(): Promise<any> {
+  return api
+    .get("/user/survey")
+    .then((response) => {
+      return Promise.resolve(response.data);
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+}
+
+// 설문조사 제출 함수
+export function postSurveyInfo({ plant, place }: postSurveyInfoProps): Promise<any> {
+  // console.log({plant, place});
+  return api
+    .post("/user/survey", { plant, place })
+    .then((response) => {
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+}
+
+// 이메일 중복 검사 함수
+export function isEmailDuplicate(email: string): Promise<boolean> {
+  return api
+    .get(`/user/email-valid?email=${email}`)
+    .then((response) => {
+      return Promise.resolve(response.data); 
+    })
+    .catch((error) => {
+      console.error('이메일 중복 검사 실패:', error);
+      return Promise.reject(error);
+    });
+}
+
+// 닉네임 중복 검사 함수
+export function isNickNameDuplicate(nickname: string): Promise<boolean> {
+  return api
+    .get(`/user/nickname-valid?nickname=${nickname}`)
+    .then((response) => {
+      return Promise.resolve(response.data);
+    })
+    .catch((error) => {
+      console.error('닉네임 중복 검사 실패:', error);
+      return Promise.reject(error);
+    });
+}
