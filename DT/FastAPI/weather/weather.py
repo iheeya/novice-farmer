1c934f48c7acfad2a29983f504bf77ede63f52d5
HokyoungKim <ssafy1123992@gmail.com>
2024-09-28 20:22:53 +0900

    [DT] Modify weather.py function
    Former-commit-id: 986692b46ac720dbe7b0327f7e7a7e851befddaa

--- DT/FastAPI/weather/weather.py
+++ DT/FastAPI/weather/weather.py
@@ -9,38 +9,34 @@ import itertools
 
 load_dotenv()
 
-url = 'https://apihub.kma.go.kr/api/typ01/url/fct_shrt_reg.php'
-params = {'tmfc' : 0, 'disp' : 1, 'authKey' : os.getenv('WEAHTER_AUTH_KEY')}
-response = requests.get(url, params=params)
+def load_reginfo():
+    url = 'https://apihub.kma.go.kr/api/typ01/url/fct_shrt_reg.php'
+    params = {'tmfc' : 0, 'disp' : 1, 'authKey' : os.getenv('WEAHTER_AUTH_KEY')}
+    response = requests.get(url, params=params)
 
-csv_data = response.text
+    csv_data = response.text
 
-# print(csv_data)
+    # print(csv_data)
 
-csv_reader = csv.reader(StringIO(csv_data))
+    csv_reader = csv.reader(StringIO(csv_data))
 
-filtered_data = []
+    filtered_data = []
 
-csv_reader = itertools.islice(csv_reader, 11, None)
+    csv_reader = itertools.islice(csv_reader, 11, None)
 
-for row in csv_reader:
-    reg = []
-    reg = list(row[0].split())
-    if len(reg) < 5:
-        continue
-    reg_id = reg[0]
-    reg_sp = reg[3]
-    reg_name = reg[4]
+    for row in csv_reader:
+        reg = []
+        reg = list(row[0].split())
+        if len(reg) < 5:
+            continue
+        reg_id = reg[0]
+        reg_sp = reg[3]
+        reg_name = reg[4]
 
-    
-    if reg_sp == 'C':
-        if '(' not in reg_name:
-            filtered_data.append([
-                str(reg_id), reg_name
-            ])
         
-
-    # with open('weather_area.csv', mode='w', newline='', encoding='utf-8-sig') as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(['id', 'name'])
-    #     writer.writerows(filtered_data)
+        if reg_sp == 'C':
+            if '(' not in reg_name:
+                filtered_data.append([
+                    str(reg_id), reg_name
+                ])
+        
