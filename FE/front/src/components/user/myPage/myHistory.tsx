a176898d29810d73cb2a6ae46e19c064efe7b783
김도환 <rlaehghkr@gmail.com>
2024-09-30 13:07:16 +0900

    [FE]feat: footer 출력 제한 구현
    Former-commit-id: 792ae6549319687dce8132c85f98afc127a5852a

--- FE/front/src/components/user/myPage/myHistory.tsx
+++ FE/front/src/components/user/myPage/myHistory.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import { getMyHistory } from "../../../services/user/myPageApi";
-import { Box, Typography, Card, CardContent, CardMedia, CircularProgress } from "@mui/material";
+import { Box, Typography, Card, CardContent, CardMedia, Button, CircularProgress } from "@mui/material";
 import { PlantProps } from "../../../services/user/myPageApi"; // 타입 가져오기
+import { useNavigate } from "react-router-dom";
 
 export default function MyHistory() {
   const [history, setHistory] = useState<PlantProps[]>([]); // 빈 배열로 초기화
   const [loading, setLoading] = useState<boolean>(true); // 로딩 상태 관리
   const [error, setError] = useState<string | null>(null); // 에러 상태 관리
+  const navigate = useNavigate();
 
   // 데이터 불러오기
   useEffect(() => {
     getMyHistory()
       .then((res) => {
-        console.log("API 응답 데이터:", res);  // 응답 데이터 확인
+        console.log("API 응답 데이터:", res); // 응답 데이터 확인
         setHistory(res); // 응답이 배열 형태라면 그대로 설정
         setLoading(false); // 로딩 상태 종료
       })
@@ -21,13 +23,22 @@ export default function MyHistory() {
         setError("데이터를 불러오는 데 실패했습니다."); // 에러 메시지 설정
         setLoading(false); // 로딩 상태 종료
       });
-  }, []); 
+  }, []);
 
   // 데이터가 없는 경우
   if (history.length === 0) {
     return (
-      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-        <Typography variant="h6">기록이 없습니다.</Typography>
+      <Box sx={{ display: "flex", flexDirection:"column",justifyContent: "center", alignItems: "center", height: "50vh" }}>
+        <Typography variant="h6">수확하신 작물이 없어요</Typography>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#5b8e55", marginTop: "3vh" }}
+          onClick={() => {
+            navigate("/register/garden");
+          }}
+        >
+          작물 등록하기
+        </Button>
       </Box>
     );
   }
@@ -42,7 +53,7 @@ export default function MyHistory() {
             component="img"
             height="140"
             // image={plant.imageurl || "/path/to/default_image.jpg"} // 기본 이미지 경로
-            alt={"식물 이미지"}            
+            alt={"식물 이미지"}
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
