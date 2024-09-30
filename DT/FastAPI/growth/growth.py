b0f78feade95ede29c48cf06f40bee5e029c876f
김호경 <ssafy1123992@gmail.com>
2024-09-30 16:33:59 +0900

    [DT] Calculate Growth Doils Base Code
    Former-commit-id: 20cf3b456c657b56df4b66a0d4b4c3db3c0fe5a7

--- DT/FastAPI/growth/growth.py
+++ DT/FastAPI/growth/growth.py
@@ -1,1 +1,49 @@
 # 작물 생장도(DDs) 계산
+import math
+
+def crops_growth(): # 현재는 토마토에 대한 값만 계산 중.
+    thi, tlow = 33.33, 7.22
+    def docalcs_S(max_val, min_val):
+        if min_val > thi:
+            heat = thi - tlow
+        else:
+            if max_val <= tlow:
+                heat = 0
+            else:
+                fk1 = 2 * tlow
+                diff = max_val - min_val
+                sum_val = max_val + min_val
+                if min_val >= tlow:
+                    heat = (sum_val - fk1) / 2
+                else:
+                    heat = sinec(sum_val, diff, fk1)
+                if max_val > thi:
+                    fk1 = 2 * thi
+                    heat -= sinec(sum_val, diff, fk1)
+        
+        ddtmp = heat / 2
+        return ddtmp
+
+    def sinec(sum_val, diff, fk1):
+        twopi = 6.28318530717959
+        pihlf = 1.5707963267949
+        d2 = fk1 - sum_val
+        d3 = diff**2 - d2**2
+        # 부동소수점 오차로 인한 sqrt 오류 방지
+        if d3 < 0 and d3 > -1e-9:
+            d3 = 0
+        theta = math.atan2(d2, math.sqrt(d3))
+        if d2 < 0 and theta > 0:
+            theta -= 3.1416
+        heat = (diff * math.cos(theta) - d2 * (pihlf - theta)) / twopi
+        return heat
+    
+    tmax, tmin = 21.56, 14.95
+    DD1 = 2*docalcs_S(tmax, tmin)
+    tmep_tlow, temp_thi = tlow, tmin
+    tlow, thi = thi, 2*thi - tlow
+    DD2 = 2*docalcs_S(tmax, tmin)
+    tlow, thi = tmep_tlow, temp_thi
+
+    DDs = round((DD1-DD2) * (9/5), 2)
+    return { 'degreeDay' : DDs}
