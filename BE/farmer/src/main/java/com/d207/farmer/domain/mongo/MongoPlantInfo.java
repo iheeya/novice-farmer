00b3093ff3b7c26ae846c1363cff7084286be27e
hajae <gkrhf@naver.com>
2024-10-04 08:55:41 +0900

    [BE]chore: mongo collection crops 도메인 구현
    Former-commit-id: 904671de33cc316a90c9287cbfe7d1e2835ea9d2

--- BE/farmer/src/main/java/com/d207/farmer/domain/mongo/MongoPlantInfo.java
+++ BE/farmer/src/main/java/com/d207/farmer/domain/mongo/MongoPlantInfo.java
@@ -1,23 +1,44 @@
 package com.d207.farmer.domain.mongo;
 
-import com.d207.farmer.dto.mongo.MongoPlantDescriptionDTO;
+import com.d207.farmer.dto.mongo.plant.FertilizerInfo;
+import com.d207.farmer.dto.mongo.plant.OptimalTemperature;
+import com.d207.farmer.dto.mongo.plant.PestOfPlant;
+import com.d207.farmer.dto.mongo.plant.Planting;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "crop")
+@Document(collection = "crops")
 public class MongoPlantInfo {
     @Id
     private String id;
+
     private String name;
-    private List<String> fertilizer;
-    private List<String> pest;
-    private List<MongoPlantDescriptionDTO> description;
+
+    private String definition;
+
+    @Field(name = "best_season")
+    private String bestSeason;
+
+    @Field(name = "optimal_temperature")
+    private OptimalTemperature optimalTemperature;
+
+    private Planting planting;
+
+    @Field(name = "fertilizer_info")
+    private FertilizerInfo fertilizerInfo;
+
+    private List<PestOfPlant> pests;
+
+    @Field(name = "additional_info")
+    private String additionalInfo;
+
 }
