73e70bc9c70e2c50f487d3ed913512ac70571c12
hajae <gkrhf@naver.com>
2024-10-04 10:39:47 +0900

    [BE]chore: mongo collection 매핑 완료
    Former-commit-id: cc53a4da768e2db6ae09a90932f5bc95a0c789fd

--- BE/farmer/src/main/java/com/d207/farmer/repository/mongo/MongoPlaceRepository.java
+++ BE/farmer/src/main/java/com/d207/farmer/repository/mongo/MongoPlaceRepository.java
@@ -4,5 +4,4 @@ import com.d207.farmer.domain.mongo.MongoPlaceInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MongoPlaceRepository extends MongoRepository<MongoPlaceInfo, String> {
-    MongoPlaceInfo findByName(String name);
 }
