c6f5794880a5e547fa8ec5a3ace6c13881ac1d49
HokyoungKim <ssafy1123992@gmail.com>
2024-10-05 20:23:49 +0900

    [DT] 작물별 DD값 계산 스케쥴러 설정
    Former-commit-id: 96ede68459e2489510eb47c97c55063daf7ef800

--- DT/FastAPI/setting/scheduler.py
+++ DT/FastAPI/setting/scheduler.py
@@ -6,12 +6,15 @@ scheduler = BackgroundScheduler()
 
 # weather과 growth에서 update 필요한 함수들 호출.
 from weather.weather import load_valinfo
+from growth.growth import calculate_crop_degree_days
 
 
 def scheduled_task():
     # 작업해야 할 함수들
     # 매일 기상 데이터 가져오기
     load_valinfo()
+    # 매일 작물별 생장도 계산하기
+    calculate_crop_degree_days()
     
 def start_scheduler():
     scheduler.add_job(scheduled_task, 'cron', hour=0, minute=0)
