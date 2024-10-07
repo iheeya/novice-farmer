c7cf98bc9d9e94a068baf94a795b4a33f32c8251
김호경 <ssafy1123992@gmail.com>
2024-10-07 15:03:19 +0900

    [DT] 서버 시작 설정 및 라우터 설정 관리
    Former-commit-id: d12ef7f21a19afb9a5fda888ad8c9b0622412de8

--- DT/FastAPI/main.py
+++ DT/FastAPI/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from contextlib import asynccontextmanager
 
 # CORS 관리
 from fastapi.middleware.cors import CORSMiddleware
+
+# 함수 관리
+from weather.weather import load_adminfo, load_areainfo, load_aswsinfo, load_curruent_special_weatherinfo, load_special_areainfo, load_valinfo
 
 # Router 관리
 from growth.routers import router as growth_router
@@ -13,17 +17,30 @@ from weather.routers import router as weather_router
 # Scheduler 관리
 from setting.scheduler import start_scheduler
 
-if __name__ == '__main__':
+    
+def start():
+    load_adminfo, load_areainfo, load_aswsinfo, load_curruent_special_weatherinfo, load_special_areainfo, load_valinfo
     # Scheduler 시작
     start_scheduler()
-
-app  = FastAPI()
+    
+# 서버 시작 시 실행할 함수 등록
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 예보구역 데이터, AWS 지점 데이터 등 필요한 함수들 자동 실행
+    start()
+    
+    yield
+    
+app  = FastAPI(lifespan=lifespan)
 
 # 허용할 도메인 목록
 origins = [
     "http://j11d207.p.ssafy.io:8081",
     "http://localhost:8000",
     "http://j11d207.p.ssafy.io:5173",
+    "https://j11d207.p.ssafy.io:8081",
+    "https://localhost:8000",
+    "https://j11d207.p.ssafy.io:5173",
 ]
 
 # CORS Middelware 설정
@@ -41,7 +58,11 @@ app.include_router(recomm_router, prefix='/data-api', tags=['recomm'])
 app.include_router(todo_router, prefix='/data-api', tags=['todo'])
 app.include_router(weather_router, prefix='/data-api', tags=['weather'])
 
+router = APIRouter()
 
 @app.get("/data-api/test")
 def read_root():
     return {"message": "연결 되었습니다!"}
+
+
+app.include_router(router)
