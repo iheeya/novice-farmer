61ed38157df6ab6044324e40e15423863a2785b0
최다희 <chdh8987@naver.com>
2024-10-07 13:27:47 +0900

    [FE]feat: 사진 미리보기
    Former-commit-id: ac8476e1c8d5731583aef372a38a3f74b7e72313

--- FE/front/src/services/CommunityArticle/ArticlePost.tsx
+++ FE/front/src/services/CommunityArticle/ArticlePost.tsx
@@ -1,21 +1,37 @@
 import axios from 'axios'
 import api from '../../utils/axios'
 
-interface CommentPost {
-    commentTitle: string,
-    commentContent: string,
-    imagePath: string[],
+interface ArticlePost {
+    communityTitle: string,
+    communityContent: string,
     communityTagList: string[],
 }
 
 
-export function CommentPost(Id:number, payload: CommentPost){ 
+export function ArticlePost(payload: ArticlePost){ 
     return api
         .post('/community', payload)
         .then((response)=>{
-            return Promise.resolve()
+            return Promise.resolve(response)
         })
         .catch((e) => {
             return Promise.reject(e)
         })
 }
+
+
+export function ArticleImgPost(Id: number, imageFile: FormData) {
+    return api
+        .post(`/community/image/${Id}`, imageFile, {
+            headers: {
+                'Content-Type': 'multipart/form-data', // 헤더에 Content-Type 설정
+            },
+        })
+        .then((response) => {
+            return Promise.resolve(response.data); // 필요한 경우 응답 데이터 반환
+        })
+        .catch((e) => {
+            return Promise.reject(e);
+        });
+}
+
