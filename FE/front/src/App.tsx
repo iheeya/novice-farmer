c9e50d611aff576cbc91b43cc9a95a60ef473e8e
김도환 <rlaehghkr@gmail.com>
2024-10-07 09:17:37 +0900

    [FE] feat: 랜딩페이지 푸터 제외
    Former-commit-id: 34b08eccfd02b3720d6e9df5af11cbada85eaf32

--- FE/front/src/App.tsx
+++ FE/front/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate, Navigate } from "react-router-dom";
 import Main from "./pages/Home/Main";
 import RegisterGarden from "./pages/AddGarden/registerGarden";
 import RegisterPlant from "./pages/AddGarden/registerPlant";
@@ -9,37 +9,65 @@ import CommunityDetail from "./pages/Community/CommunityDetail";
 import SignUp from "./pages/user/signUp";
 import Login from "./pages/user/login";
 import Survey from "./pages/user/survey";
-import MyGarden from "./pages/Detail/myGarden";  
+import MyGarden from "./pages/Detail/myGarden";
 import MyPlant from "./pages/Detail/myPlant";
 import MyPage from "./pages/user/myPage";
 import ProfileUpdate from "./pages/user/profileUpdate";
-import CameraPage from "./pages/Detail/cameraPage"; // 새 페이지 컴포넌트
-import FooterWithLocation from "./components/FooterWithLocation"; // Footer 로직 분리
+import CameraPage from "./pages/Detail/cameraPage";
+import FooterWithLocation from "./components/FooterWithLocation";
 import HomePage from "./pages/Home/Landing";
 import InfoHome from "./pages/Information/InfoHome";
+// AppWrapper 컴포넌트
+function AppWrapper() {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-function App() {
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    console.log("accessToken", accessToken);
+
+    // 인증이 필요 없는 경로 리스트
+    const publicPaths = ["/user/login", "/user/signup", "/introduce"];
+
+    // 현재 경로가 인증이 필요 없는 경로가 아닌데, 토큰이 없으면 로그인 페이지로 이동
+    if (!accessToken && !publicPaths.includes(location.pathname)) {
+      navigate("/introduce");
+    }
+  }, [location, navigate]);
+
   return (
-    <Router>
+    <>
       <Routes>
+        {/* 공개 페이지들 */}
         <Route path="/" element={<Main />} />
+        <Route path="/user/signup" element={<SignUp />} />
+        <Route path="/user/login" element={<Login />} />
+        <Route path="/user/survey" element={<Survey />} />
+        <Route path="/introduce" element={<HomePage />} />
+
+        {/* 인증이 필요한 페이지들 */}
         <Route path="/register/garden" element={<RegisterGarden />} />
         <Route path="/register/plant" element={<RegisterPlant />} />
         <Route path="/weekendFarm-recommend" element={<WeekendFarmList />} />
         <Route path="/community" element={<Community />} />
         <Route path="/community/:id/detail" element={<CommunityDetail />} />
-        <Route path="/user/signup" element={<SignUp />} />
-        <Route path="/user/login" element={<Login />} />
-        <Route path="/user/survey" element={<Survey />} />
         <Route path="/myGarden/:myPlaceId" element={<MyGarden />} />
         <Route path="/myGarden/:myPlaceId/:myPlantId" element={<MyPlant />} />
         <Route path="/myPage" element={<MyPage />} />
         <Route path="/myPage/profile" element={<ProfileUpdate />} />
         <Route path="/myGarden/:myPlaceId/:myPlantId/camera" element={<CameraPage />} />
-        <Route path="/introduce" element={<HomePage />} />
         <Route path="/info" element={<InfoHome/>} />
       </Routes>
       <FooterWithLocation />
+    </>
+  );
+}
+
+// 최상위 App 컴포넌트
+function App() {
+  return (
+    <Router>
+      <AppWrapper />
     </Router>
   );
 }
