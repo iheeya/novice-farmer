1ffa22b70faf46a30789d255c1f9e98388dd08f9
최다희 <chdh8987@naver.com>
2024-10-10 09:56:14 +0900

    [FE]feat: 커뮤니티 뒤로가기 버튼
    Former-commit-id: 39be284ac66cd227b742ef3026ad9d4c18af993d

--- FE/front/src/components/CommunityDetail/CommunityDetailHeader.tsx
+++ FE/front/src/components/CommunityDetail/CommunityDetailHeader.tsx
@@ -1,6 +1,7 @@
 import '../../styles/CommunityDetail/CommunityDetailHeader.css'
 import { useNavigate, useParams } from 'react-router-dom';
 import { FaSearch } from "react-icons/fa";
+import Back from '../../assets/icons/Back.png'
 
 
 function CommunityDetailHeader(){
@@ -15,10 +16,14 @@ function CommunityDetailHeader(){
         navigate('/community/search')
     }
 
+    const handleBack = () => {
+        navigate(-1)
+    }
 
 
     return(
         <div className='community-header-container'>
+            <div className='back-box' onClick={handleBack}><img className='back' src={Back} alt='뒤로가기 버튼'/></div>
             <FaSearch className='search' onClick={handleSearchPage}/>   {/* 누르면 검색 페이지로 이동*/}
             <div className='my-profile' onClick={handleMyPage}>MY</div> {/* 누르면 마이페이지로 이동*/}
         </div> 
--- FE/front/src/components/WirteArticle/ReWriteHeader.tsx
+++ FE/front/src/components/WirteArticle/ReWriteHeader.tsx
@@ -1,7 +1,20 @@
+import Back from '../../assets/icons/Back.png'
+import { useNavigate } from 'react-router-dom';
 
 function ReWriteHeader(){
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate(-1)
+    }
+    
     return (
-        <div style={{display: "flex", marginTop: "10%", justifyContent: "center", color:"#5B8E55", fontSize: '1.3rem'}}>수정하기</div>
+        <div style={{ display: "flex", alignItems: "center", marginTop: "10%", marginLeft:'5%' }}>
+        <div onClick={handleBack} style={{ marginRight: '5%' }}> {/* 이미지와 글 사이 간격 추가 */}
+          <img src={Back} alt="뒤로가기 버튼" style={{width: '40%'}}/>
+        </div>
+        <div style={{ color: "#5B8E55", fontSize: "1.3rem" }}>수정하기</div>
+      </div>
     )
 }
 
